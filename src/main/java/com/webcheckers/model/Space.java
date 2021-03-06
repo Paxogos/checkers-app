d2cc77f92a701e5e025f9c35c878f3377f507664
Jack Sipley <69880845+jrs8215@users.noreply.github.com>
2021-03-06 16:09:34 -0500

    Space has-a Piece and spaceColor to tell what type of space it is. isValid() checks to see if the space is DARK and no pieces in square.

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -2,21 +2,42 @@ package com.webcheckers.model;
 
 public class Space {
 
+    public enum SpaceColor{LIGHT, DARK}
+
     // must be 0 to 7
     private int cellIdx;
+    private Piece piece;
+    private SpaceColor spaceColor;
+
+    public Space(int cellIdx, Piece piece) {
+        this.cellIdx = cellIdx;
+        this.piece = piece;
+
+        /*
+        Dark peices will generate as odds and Light pieces will generate as even, while building the board
+        in an S-shape
+         */
+        if(cellIdx % 2 != 0)
+            spaceColor = SpaceColor.DARK;
+        else
+            spaceColor = SpaceColor.LIGHT;
+    }
+
+    /**
+     *
+     * @return true if space is vacant, and if the space is a dark square
+     */
+    public boolean isValid(){
+        return piece == null && spaceColor == SpaceColor.DARK;
+
+    }
 
     public int getCellIdx() {
         return cellIdx;
     }
 
-    public boolean isValid(){
-
-        return false;
-    }
-
     public Piece getPiece(){
-
-        return null;
+        return piece;
     }
 
 }
