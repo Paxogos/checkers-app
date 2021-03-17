88148ab95ca472bc1499f13515548b7b22232928
Jack Sipley <69880845+jrs8215@users.noreply.github.com>
2021-03-17 12:52:24 -0400

    Instantiated Position

--- src/main/java/com/webcheckers/model/Position.java
+++ src/main/java/com/webcheckers/model/Position.java
@@ -1,4 +1,18 @@
 package com.webcheckers.model;
 
 public class Position {
+    //attributes
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    public int getRow() {
+        return row;
+    }
+    public int getColumn() {
+        return column;
+    }
 }
