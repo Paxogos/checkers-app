89b6d85a91d173745b1d04d5d9fd1f4fb066b5f2
Jack Sipley <69880845+jrs8215@users.noreply.github.com>
2021-03-21 17:29:54 -0400

    Added test for isKing() and isSingle() getters

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -39,4 +39,69 @@ public class PieceTest {
         //Analyze stats of CuT, should return "Piece: {WHITE, KING}"
         assertEquals(CuT.toString(), "Piece: {" + CuT.getColor() + ", " + CuT.getType() + "}");
     }
+
+    @Test
+    void getter_isSingle_red_true() {
+        //Create a RED Piece as type SINGLE
+        Piece CuT = new Piece(Piece.Color.RED, Piece.Type.SINGLE);
+        //should return true since CuT is a type SINGLE
+        assertTrue(CuT.isSingle(), "Piece is type SINGLE");
+    }
+
+    @Test
+    void getter_isSingle_white_true() {
+        //Create a WHITE Piece as type SINGLE
+        Piece CuT = new Piece(Piece.Color.WHITE, Piece.Type.SINGLE);
+        //should return true since CuT is a type SINGLE
+        assertTrue(CuT.isSingle(), "Piece is type SINGLE");
+    }
+
+    @Test
+    void getter_isSingle_red_false() {
+        //Create a RED Piece as type KING
+        Piece CuT = new Piece(Piece.Color.RED, Piece.Type.KING);
+        //should return false since CuT isn't type SINGLE
+        assertFalse(CuT.isSingle(), "Piece isn't type SINGLE");
+    }
+
+    @Test
+    void getter_isSingle_white_false() {
+        //Create a WHITE Piece as type KING
+        Piece CuT = new Piece(Piece.Color.WHITE, Piece.Type.KING);
+        //should return false since CuT isn't type SINGLE
+        assertFalse(CuT.isSingle(), "Piece isn't type SINGLE");
+    }
+
+    @Test
+    void getter_isKing_red_true() {
+        //Create a RED Piece as type KING
+        Piece CuT = new Piece(Piece.Color.RED, Piece.Type.KING);
+        //Should hold true since CuT is a KING
+        assertTrue(CuT.isKing(), "Piece is type KING");
+    }
+
+    @Test
+    void getter_isKing_white_true() {
+        //Create a WHITE Piece as type KING
+        Piece CuT = new Piece(Piece.Color.WHITE, Piece.Type.KING);
+        //Should hold true since CuT is a KING
+        assertTrue(CuT.isKing(), "Piece is type KING");
+    }
+
+    @Test
+    void getter_isKing_red_false() {
+        //Create a RED Piece as type KING
+        Piece CuT = new Piece(Piece.Color.RED, Piece.Type.SINGLE);
+        //Should hold true since CuT is a KING
+        assertFalse(CuT.isKing(), "Piece isn't type KING");
+    }
+
+    @Test
+    void getter_isKing_white_false() {
+        //Create a WHITE Piece as type KING
+        Piece CuT = new Piece(Piece.Color.WHITE, Piece.Type.SINGLE);
+        //Should hold true since CuT is a KING
+        assertFalse(CuT.isKing(), "Piece isn't type KING");
+    }
+
 }
