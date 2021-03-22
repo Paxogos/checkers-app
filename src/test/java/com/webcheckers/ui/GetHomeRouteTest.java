7fa160b5d43da79724044e0fc1a131c08a912e2c
Jack Sipley <69880845+jrs8215@users.noreply.github.com>
2021-03-21 23:45:16 -0400

    More work added towards GetHomeRouteTest

--- src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
@@ -1,5 +1,56 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.application.GameCenter;
+import com.webcheckers.application.PlayerLobby;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import spark.*;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@Tag("UI-tier")
 public class GetHomeRouteTest {
+
+    private GetHomeRoute CuT;
+
+    //mock objects to help with tests
+    private GameCenter gameCenter;
+    private PlayerLobby playerLobby;
+    private TemplateEngine templateEngine;
+
+    private Request request;
+    private Session session;
+    private Response response;
+
+    @BeforeEach
+    //Creating mock objects to run with the test
+    public void setup() {
+        session = mock(Session.class);
+        response = mock(Response.class);
+        templateEngine = mock(TemplateEngine.class);
+
+        gameCenter = new GameCenter();
+        playerLobby = new PlayerLobby();
+
+        CuT = new GetHomeRoute(playerLobby, gameCenter, templateEngine);
+    }
+
+    public void new_session() {
+        final TemplateEngineTester engineTester = new TemplateEngineTester();
+        when(templateEngine.render(any(ModelAndView.class))).thenAnswer(engineTester.makeAnswer());
+
+        //make the handler CuT to start the test
+        CuT.handle(request, response);
+
+        engineTester.assertViewModelExists();
+        engineTester.assertViewModelIsaMap();
+
+        engineTester.assertViewModelAttribute(GetHomeRoute.PLAYER_ATTR, GetHomeRoute.AVAILABLE_PLAYER_LIST_ATTR);
+        engineTester.assertViewModelAttribute(GetHomeRoute.LOBBY_SIZE_ATTR, GetHomeRoute.BUSY_PLAYER_LIST_ATTR);
+
+    }
 
 }
