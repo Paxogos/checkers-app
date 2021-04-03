116a56daeb39c59f45406a60778aa7246eb07bdd
ttt8000 <ttt8000@g.rit.edu>
2021-04-03 16:16:51 -0400

    Made canJumpForward() and canJump() classes in Piece

--- src/main/java/com/webcheckers/model/Single.java
+++ src/main/java/com/webcheckers/model/Single.java
@@ -43,55 +43,8 @@ public class Single extends Piece{
 
     }
 
-    /**
-     * Checks if this single can jump in any direction
-     *
-     * @param currentPosition       current position of the piece
-     * @param board                 current configuration of the board
-     * @return  true if piece can legally jump another piece
-     */
+    @Override
     public boolean canJump(Position currentPosition, Board board) {
-
-        System.out.println("Position of last piece: " + currentPosition);
-        boolean canJumpRight;
-        boolean canJumpLeft;
-
-        // Corrects the direction of movement based on piece color
-        // Red technically moves backward, white moves forward
-        int directionCorrector = -1;
-        if (this.getColor() == Color.WHITE)
-            directionCorrector = 1;
-
-        //Distance in each direction for a jump
-        int deltaY = 2 * directionCorrector;
-        int deltaX = 2 * directionCorrector;
-
-        int currentRow = currentPosition.getRow();
-        int currentCell = currentPosition.getCell();
-
-        // Checks if a jump to the left or right is out of bounds in the y-direction
-        if (currentRow + deltaY >= Board.GRID_LENGTH || currentRow + deltaY < 0)
-            return false;
-
-        // Checks if a jump to the left is out of bounds in the x-direction
-        if (currentCell + deltaX >= Board.GRID_LENGTH || currentCell + deltaX < 0)
-            canJumpLeft = false;
-        else {
-        Position leftJump = new Position (currentRow + deltaY, currentCell + deltaX);
-        Position leftJumpCapture = new Move(currentPosition, leftJump).midpoint();
-        canJumpLeft = !board.getSpace(leftJump).isOccupied() && board.getPieceAt(leftJumpCapture) != null &&
-                board.getPieceAt(leftJumpCapture).getColor() != this.getColor(); }
-
-        // Checks if a jump to the right is out of bounds in the x-direction
-        if (currentCell - deltaX >= Board.GRID_LENGTH || currentCell - deltaX < 0)
-            canJumpRight = false;
-        else {
-        Position rightJump = new Position (currentRow + deltaY, currentCell - deltaX);
-        Position rightJumpCapture = new Move(currentPosition, rightJump).midpoint();
-        canJumpRight = !board.getSpace(rightJump).isOccupied() && board.getPieceAt(rightJumpCapture) != null &&
-                board.getPieceAt(rightJumpCapture).getColor() != this.getColor(); }
-
-
-        return canJumpLeft || canJumpRight;
+        return this.canJumpForward(currentPosition, board);
     }
 }
